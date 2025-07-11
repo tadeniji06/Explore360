22c554604ac216782b294ebc38be84f3b1157b68
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-07-11 13:36:34 +0100

    Add @microsoft/clarity dependency and initialize in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import AppRoutes from "./routes/routes";
+import { useEffect } from "react";
+import Clarity from "@microsoft/clarity";
 
 const App = () => {
+	const projectId = 'sd5vdec7d7';
+		useEffect(() => {
+		Clarity.init(projectId);
+	}, []);
 	return <AppRoutes />;
 };
 export default App;
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 const AppLayout = () => {
+	const location = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location]);
 	return (
 		<>
 			<Header />
